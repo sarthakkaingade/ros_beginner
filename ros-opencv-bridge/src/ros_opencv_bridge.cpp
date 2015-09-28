20688d5bfd065c94024dac8ec13c4629697ec964
Sarthak Kaingade <sarthakkaingade@yahoo.com>
2015-09-27 21:30:20 -0400

    Moved circle to center of image

--- ros-opencv-bridge/src/ros_opencv_bridge.cpp
+++ ros-opencv-bridge/src/ros_opencv_bridge.cpp
@@ -45,7 +45,7 @@ class ImageConverter
 
 		// Draw an example circle on the video stream
 		if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-		cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+		cv::circle(cv_ptr->image, cv::Point(cv_ptr->image.cols/2, cv_ptr->image.rows/2), 30, CV_RGB(255,0,0));
 		
 		// Update GUI Window
 		cv::imshow(OPENCV_WINDOW, cv_ptr->image);
